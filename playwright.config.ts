8f49b411681f0f69328182902899c410b2c665f7
Leslaw Kula <it@kula.wroclaw.pl>
2024-06-16 20:46:46 +0200

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ enum ENVIRONMENTS {
   prod = "Prod",
 }
 // Choosing the default environment
-const defaultEnvironment = ENVIRONMENTS.stage;
+const defaultEnvironment = ENVIRONMENTS.stageDev;
 
 const environment = process.env.ENV ?? defaultEnvironment;
 dotenv.config({
@@ -37,13 +37,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: process.env.BASE_URL,
-
+    screenshot: 'only-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     video: 'off',
--- tests/generateAgreement.spec.ts
+++ tests/generateAgreement.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable playwright/expect-expect */
 import { test, expect } from '@playwright/test';
 
 import { basicSteps } from './steps/basicSteps';
@@ -13,40 +14,63 @@ test.only('Generate 5.11 Program Agreement', async ({ page }) => {
     const selectedTrainingDetailsId = 2;
     const selectedPersonalDataId = 0;
 
-    expect(1).toBe(1);
     const agreementNumberMiddlePart = createAgreementNumberMiddlePart();
-
-    await basicSteps().loginToApp(page, {
-        user: process.env.TEST_ADMIN_USER as string,
-        password: process.env.TEST_ADMIN_PASSWORD as string,
+    await test.step('Login to app step', async () => {
+        await basicSteps().loginToApp(page, {
+            user: process.env.TEST_ADMIN_USER as string,
+            password: process.env.TEST_ADMIN_PASSWORD as string,
+        });
     });
 
-    await trainingDetailsSteps().reandAndCheckTrainingDataStep(page, {
-        program: selectedProgram,
-        year: selectedYear,
-        trainingDetailsId: selectedTrainingDetailsId,
+    await test.step('Fetch data from BUR and check it step', async () => {
+        await trainingDetailsSteps().reandAndCheckTrainingDataStep(page, {
+            program: selectedProgram,
+            year: selectedYear,
+            trainingDetailsId: selectedTrainingDetailsId,
+        });
     });
-    await trainingCompanySteps().checkTrainingCompanyStep(page, test, {
-        trainingDetailsId: selectedTrainingDetailsId,
-    });
-    await trainingBeneficiarySteps().fillParticipantDataAndCheckStep(
-        page,
-        test,
-        {
-            personalDataId: selectedPersonalDataId,
-        },
-    );
-    await agreementDetailsSteps().fillAgreementDetailsAndCheckStep(page, test, {
-        trainingDetailsId: selectedTrainingDetailsId,
-        agreementNumberMiddlePart: agreementNumberMiddlePart,
-    });
-    await agreementDetailsSteps().checkFilledAgreementPreviewStep(page, test, {
-        trainingDetailsId: selectedTrainingDetailsId,
-        personalDataId: selectedPersonalDataId,
-        agreementNumberMiddlePart: agreementNumberMiddlePart,
-        programNumber: selectedProgram,
-    });
-    await agreementDetailsSteps().generateAgreementsStep(page, test);
 
-    // await page.close();
+    await test.step('Check data regarding training company fetched from BUR and REGON api step', async () => {
+        await trainingCompanySteps().checkTrainingCompanyStep(page, test, {
+            trainingDetailsId: selectedTrainingDetailsId,
+        });
+    });
+
+    await test.step('Fill participant details and check it step', async () => {
+        await trainingBeneficiarySteps().fillParticipantDataAndCheckStep(
+            page,
+            test,
+            {
+                personalDataId: selectedPersonalDataId,
+            },
+        );
+    });
+
+    await test.step('Fill agreement details and check it step', async () => {
+        await agreementDetailsSteps().fillAgreementDetailsAndCheckStep(
+            page,
+            test,
+            {
+                trainingDetailsId: selectedTrainingDetailsId,
+                agreementNumberMiddlePart: agreementNumberMiddlePart,
+            },
+        );
+    });
+    await test.step('Check agreement summary step', async () => {
+        await agreementDetailsSteps().checkFilledAgreementPreviewStep(
+            page,
+            test,
+            {
+                trainingDetailsId: selectedTrainingDetailsId,
+                personalDataId: selectedPersonalDataId,
+                agreementNumberMiddlePart: agreementNumberMiddlePart,
+                programNumber: selectedProgram,
+            },
+        );
+    });
+    await test.step('Generate agreement step', async () => {
+        await agreementDetailsSteps().generateAgreementsStep(page, test);
+    });
+
+    await page.close();
 });
